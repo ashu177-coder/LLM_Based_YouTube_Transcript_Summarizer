d63c9c4aaa542ea3aed21f7f02d0d81e42fca0d5
AshuMishra <ashutoshsmishra007@gmail.com>
2024-03-19 01:27:08 +0530

    Project files added

---
+++ app.py
@@ -0,0 +1,60 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import google.generativeai as genai
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+# load all the environment variables
+load_dotenv()
+
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+# getting the transcript data from yt videos
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id = youtube_video_url.split("=")[1]
+
+        transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+        return transcript
+
+    except Exception as e:
+        raise e
+
+# getting the summary based on Prompt from Google Gemini Pro
+
+
+def generate_gemini_content(transcript_text, prompt):
+
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt+transcript_text)
+    return response.text
+
+
+prompt = """You are Yotube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+within 250 words. Please provide the summary of the text given here:  """
+
+
+st.title("YouTube Transcript to Detailed Notes Converter")
+youtube_link = st.text_input("Enter YouTube Video Link:")
+
+if youtube_link:
+    video_id = youtube_link.split("=")[1]
+    print(video_id)
+    st.image(
+        f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+
+if st.button("Get Detailed Notes"):
+    transcript_text = extract_transcript_details(youtube_link)
+
+    if transcript_text:
+        summary = generate_gemini_content(transcript_text, prompt)
+        st.markdown("## Detailed Notes:")
+        st.write(summary)
